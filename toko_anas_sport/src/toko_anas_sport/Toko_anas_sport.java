41bc2e6da1e454b88b496bc65f13963235dbc3c9
syafiraaudina <162035614+syafiraaudina@users.noreply.github.com>
2024-03-03 21:53:50 +0700

    Add files via upload

---
+++ toko_anas_sport/src/toko_anas_sport/Toko_anas_sport.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package toko_anas_sport;
+
+/**
+ *
+ * @author W10
+ */
+public class Toko_anas_sport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
